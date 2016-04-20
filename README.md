7bf33985e60d3d8a7442fea57209222c292d8251
肥肥兔 <531041610@qq.com>
2016-04-20 21:19:18 +0800

    chang some code

--- README.md
+++ README.md
@@ -13,24 +13,36 @@ python作为动态语言，他的方便和灵活是无需置疑的。但也是�
 
 
 在python中检测函数参数类型的时候你可以用```isinstance```来处理，如果是参数数量太多，你也想约束,那么
-用```isinstance```判断会显得很烦，如果能像```java```,```C++```这类静态语言那样在声明函数时就添能加约束那该多好。
+用```isinstance```判断会显得很烦，如果能像```java```,```C++```这类静态语言那样在声明函数时就指定参数类型那该多好。
 
 于是脑洞打开，花费了半小时，写了这个装饰器，约束了函数的参数类型和返回类型。
+
+成员名单
+----------
+
+	#使用期间可能会触发的异常类型
+	LimitError
+
+	#装饰器，对被修饰的函数的参数类型，返回值等进行约束
+	@type_limit
+
+	#装饰器，对被修饰的函数中列表形参数的最少长度进行约束，避免运行是触发越界异常
+	@list_limit
 
 
 代码示例
 -------
 
 
-    from pylimit import LimitType
+    from pylimit import type_limit
     
-    @LimitType(int,int)
+    @type_limit(int,int)
     def test(x,y):
         return x + y
 
 当然你也可以对返回值也进行约束
 
-    @LimitType(int,int,returnType = int)
+    @type_limit(int,int,returnType = int)
     def test2(x,y):
         return x + y
 
@@ -39,7 +51,7 @@ python作为动态语言，他的方便和灵活是无需置疑的。但也是�
     
 	print(test(1,'2'))
     
-    >>> pylimit.LimitError: The param 2 should be <class 'type'>,but it's <class 'str'> now!
+    >>> pylimit.LimitError: The param 2 should be <class 'int'>,but it's <class 'str'> now!
     
 正常情况下
 
@@ -61,9 +73,12 @@ To Do List
 ---------
 
 - [x] 函数参数类型，返回值进行约束
-- [ ] 一维列表的作参数时，对最少长度进行约束,避免运行时才触发越界异常
-- [ ] 二维列表or 元组每一维的长度进行约束，某些算法可能需要，例如决策树的训练
-- [ ] 等哪天我发现python某个方面让我不爽的时候再添加到To Do 
+- [x] 一维列表的作参数时，对最少长度进行约束,避免运行时才触发越界异常
+- [ ] 二维列表or 元组每一维的长度进行约束(长度必须相等)，某些算法可能需要，例如决策树的训练
+- [ ] 列表的长度，每一个位置固定一个指定类型，如规定一个list或tuple的类型为[int,float,str]等有序,同样是出于某些算法的需求。
+- [ ] 真正意义上的常量,即Const
+
+等哪天我发现python某个方面让我不爽的时候再添加到To Do 
 
 
 
--- pylimit.py
+++ pylimit.py
@@ -5,14 +5,14 @@ class LimitError(Exception):
 		self._message = str(message)
 
 	def __str__(self):
-		return self._message
+		return self.__repr__()
 
 	def __repr__(self):
 		return self._message
 
 #利用装饰器限制python的参数数据类型
-def LimitType(*typeLimit,**returnType):
-	def testValueType(func):
+def type_limit(*typeLimit,**returnType):
+	def test_value_type(func):
 		def wrapper(*param,**kw):
 			length = len(typeLimit)
 			if length != len(param):
@@ -21,7 +21,7 @@ def LimitType(*typeLimit,**returnType):
 				t = typeLimit[index]
 				p = param[index]
 				if not isinstance(p,t):
-					raise LimitError("The param %s should be %s,but it's %s now!"%(str(p),type(t),type(p)))  
+					raise LimitError("The param %s should be %s,but it's %s now!"%(str(p),type(t()),type(p)))  
 			res = func(*param,**kw)
 			if "returnType" in returnType:
 				limit = returnType["returnType"]
@@ -29,6 +29,34 @@ def LimitType(*typeLimit,**returnType):
 					raise LimitError("This function must return a value that is %s,but now it's %s"%(limit,type(res) ) )
 			return res
 		return wrapper
-	return testValueType
+	return test_value_type
 
+
+#使用装饰器指定某个以列表或元组为参数时，限制指定长度，不够长则自动补充，可指定补充的元素
+#info[value] =  length
+#info[supp] = None #补充元素
+def list_limit(*var,**info):
+	default = info.get("supp",None)
+	var_name_list = [ value  for value in var]
+	def test_length(func):
+		def wrapper(*param,**kw):
+			param_length = len(param)
+			if param_length != len(var_name_list):
+				raise LimitError("The number of param in this function must same length as "\
+				    "the number of param that you want to limit!")
+			for index in range(param_length):
+				var_name = var_name_list[index]
+				#最小长度限制
+				min_length = info.get(var_name,0)
+				var_value = param[index]
+				if not isinstance(var_value,list):
+					raise LimitError("You can only limit the param,which is a list!Type of %s"\
+					        " is %s"%(var_value,type(var_value)))
+				#print("name: %s  value: %s   min_length:%s"%(var_name,var_value,min_length))
+				now_length = len(var_value)
+				if now_length < min_length:
+					var_value += [default for num in range(min_length-now_length)]
+			return func(*param,**kw)
+		return wrapper
+	return test_length
 
