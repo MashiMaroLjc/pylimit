038c5ec6adf591e6106b5f80e794ed308c18bd84
肥肥兔 <531041610@qq.com>
2016-04-21 18:15:08 +0800

    update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+VERSION = "0.5"
+AUTHOR = "fat_rabbit"
+
+
+setup(
+    name="pylimit",
+    version=VERSION,
+    author=AUTHOR,
+    license="Apache License 2.0",
+    url="https://github.com/MashiMaroLjc/pylimit",
+	py_modules=['pylimit'],
+    zip_safe=False,
+)
